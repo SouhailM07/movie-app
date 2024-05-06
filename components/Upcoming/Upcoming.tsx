5ff94963fdcfc585fd1d7fa8f62d9020e8bb216f
shadow <lightshadow416@gmail.com>
2024-05-06 16:44:23 +0200

    [Upcoming] component was created

---
+++ components/Upcoming/Upcoming.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { upcomingStyles } from "./upcomingStyles.ts";
+import tw from "../../lib/tailwind.js";
+export default function Upcoming() {
+  return (
+    <>
+      <View>
+        <Text>Upcoming</Text>
+      </View>
+    </>
+  );
+}
