5ca6735e64ecb70bfb37f2bba928466e5cd475d9
shadow <lightshadow416@gmail.com>
2024-05-06 21:56:03 +0200

    [Sidebar] component was created

---
+++ components/Sidebar/Sidebar.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { sidebarStyles } from "./sidebarStyles.ts";
+import tw from "../../lib/tailwind.js";
+export default function Sidebar() {
+  return (
+    <>
+      <View>
+        <Text>Sidebar</Text>
+      </View>
+    </>
+  );
+}
