c8fe86a996d7b178d44087f56bba34c5db9285fd
shadow <lightshadow416@gmail.com>
2024-05-06 11:53:13 +0200

    [MyContainer] component was created

---
+++ components/MyContainer/MyContainer.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { mycontainerStyles } from "./mycontainerStyles.ts";
+import tw from "../../lib/tailwind.js";
+export default function MyContainer() {
+  return (
+    <>
+      <View>
+        <Text>MyContainer</Text>
+      </View>
+    </>
+  );
+}
