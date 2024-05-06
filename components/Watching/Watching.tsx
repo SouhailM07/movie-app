007d58ec9faa0528939d583862bd07a8ca41f0cd
shadow <lightshadow416@gmail.com>
2024-05-06 17:26:41 +0200

    [Watching] component was created

---
+++ components/Watching/Watching.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { watchingStyles } from "./watchingStyles.ts";
+import tw from "../../lib/tailwind.js";
+export default function Watching() {
+  return (
+    <>
+      <View>
+        <Text>Watching</Text>
+      </View>
+    </>
+  );
+}
