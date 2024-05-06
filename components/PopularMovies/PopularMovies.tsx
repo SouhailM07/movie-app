7b3fca061cb25d2b305765d5b9495e5792f29401
shadow <lightshadow416@gmail.com>
2024-05-06 16:15:46 +0200

    [PopularMovies] component was created

---
+++ components/PopularMovies/PopularMovies.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { popularmoviesStyles } from "./popularmoviesStyles.ts";
+import tw from "../../lib/tailwind.js";
+export default function PopularMovies() {
+  return (
+    <>
+      <View>
+        <Text>PopularMovies</Text>
+      </View>
+    </>
+  );
+}
