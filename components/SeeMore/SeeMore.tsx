0eae324d9b9188fe085f3d04b99e0f2719985007
shadow <lightshadow416@gmail.com>
2024-05-06 17:59:25 +0200

    [SeeMore] component was created

---
+++ components/SeeMore/SeeMore.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { seemoreStyles } from "./seemoreStyles.ts";
+import tw from "../../lib/tailwind.js";
+export default function SeeMore() {
+  return (
+    <>
+      <View>
+        <Text>SeeMore</Text>
+      </View>
+    </>
+  );
+}
