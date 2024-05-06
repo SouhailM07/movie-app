97a493dcd67a4f5e8bc33c16ec306ac904f064f3
shadow <lightshadow416@gmail.com>
2024-05-06 20:02:32 +0200

    [SelectedContent] component was created

---
+++ components/SelectedContent/SelectedContent.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { selectedcontentStyles } from "./selectedcontentStyles.ts";
+import tw from "../../lib/tailwind.js";
+export default function SelectedContent() {
+  return (
+    <>
+      <View>
+        <Text>SelectedContent</Text>
+      </View>
+    </>
+  );
+}
