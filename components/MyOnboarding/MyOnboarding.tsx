25a82955448bd42f3f90afa9be1ae1d07b9221fd
shadow <lightshadow416@gmail.com>
2024-05-06 12:26:54 +0200

    [MyOnboarding] component was created

---
+++ components/MyOnboarding/MyOnboarding.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { myonboardingStyles } from "./myonboardingStyles.ts";
+import tw from "../../lib/tailwind.js";
+export default function MyOnboarding() {
+  return (
+    <>
+      <View>
+        <Text>MyOnboarding</Text>
+      </View>
+    </>
+  );
+}
