e106c82a185a685f1f52c4ecc9eb986ed74d54ee
shadow <lightshadow416@gmail.com>
2024-05-06 12:59:55 +0200

    [Home] component was created

---
+++ components/Home/Home.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { homeStyles } from "./homeStyles.ts";
+import tw from "../../lib/tailwind.js";
+export default function Home() {
+  return (
+    <>
+      <View>
+        <Text>Home</Text>
+      </View>
+    </>
+  );
+}
