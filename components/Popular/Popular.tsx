982590968eac8706b9b3849eb80cdcef15690035
shadow <lightshadow416@gmail.com>
2024-05-06 16:29:20 +0200

    [Popular] component was created

---
+++ components/Popular/Popular.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { popularStyles } from "./popularStyles.ts";
+import tw from "../../lib/tailwind.js";
+export default function Popular() {
+  return (
+    <>
+      <View>
+        <Text>Popular</Text>
+      </View>
+    </>
+  );
+}
