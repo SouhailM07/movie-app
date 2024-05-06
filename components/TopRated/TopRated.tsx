de68b1f205dcfe8e8f75eaceafcc0766b24f6ba1
shadow <lightshadow416@gmail.com>
2024-05-06 16:33:23 +0200

    [TopRated] component was created

---
+++ components/TopRated/TopRated.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { topratedStyles } from "./topratedStyles.ts";
+import tw from "../../lib/tailwind.js";
+export default function TopRated() {
+  return (
+    <>
+      <View>
+        <Text>TopRated</Text>
+      </View>
+    </>
+  );
+}
