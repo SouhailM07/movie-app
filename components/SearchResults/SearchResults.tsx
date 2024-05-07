99e1d1a7500ee9a5403db60134ced9e848a74d94
shadow <lightshadow416@gmail.com>
2024-05-07 18:46:54 +0200

    [SearchResults] component was created

---
+++ components/SearchResults/SearchResults.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { searchresultsStyles } from "./searchresultsStyles.ts";
+import tw from "../../lib/tailwind.js";
+export default function SearchResults() {
+  return (
+    <>
+      <View>
+        <Text>SearchResults</Text>
+      </View>
+    </>
+  );
+}
