97f86cf43f7f8fa54f6dd93c31e47dc3f5d4d1c0
shadow <lightshadow416@gmail.com>
2024-05-07 14:31:58 +0200

    [FavoriteList] component was created

---
+++ components/FavoriteList/FavoriteList.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { favoritelistStyles } from "./favoritelistStyles.ts";
+import tw from "../../lib/tailwind.js";
+export default function FavoriteList() {
+  return (
+    <>
+      <View>
+        <Text>FavoriteList</Text>
+      </View>
+    </>
+  );
+}
