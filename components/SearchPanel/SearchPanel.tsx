b18ce3b24edf69e14ef483a585233223abcb5ccb
shadow <lightshadow416@gmail.com>
2024-05-07 14:32:56 +0200

    [SearchPanel] component was created

---
+++ components/SearchPanel/SearchPanel.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { searchpanelStyles } from "./searchpanelStyles.ts";
+import tw from "../../lib/tailwind.js";
+export default function SearchPanel() {
+  return (
+    <>
+      <View>
+        <Text>SearchPanel</Text>
+      </View>
+    </>
+  );
+}
