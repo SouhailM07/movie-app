50d293a85fafeb8ce01b1ee934e1d5b8ba63ee87
shadow <lightshadow416@gmail.com>
2024-05-07 14:11:06 +0200

    [BottomNavbar] component was created

---
+++ components/BottomNavbar/BottomNavbar.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { bottomnavbarStyles } from "./bottomnavbarStyles.ts";
+import tw from "../../lib/tailwind.js";
+export default function BottomNavbar() {
+  return (
+    <>
+      <View>
+        <Text>BottomNavbar</Text>
+      </View>
+    </>
+  );
+}
