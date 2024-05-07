b885562d7c6cbdbca4200c3da6efb4d31b78ee8b
shadow <lightshadow416@gmail.com>
2024-05-07 14:29:57 +0200

    [WatchList] component was created

---
+++ components/WatchList/WatchList.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { watchlistStyles } from "./watchlistStyles.ts";
+import tw from "../../lib/tailwind.js";
+export default function WatchList() {
+  return (
+    <>
+      <View>
+        <Text>WatchList</Text>
+      </View>
+    </>
+  );
+}
