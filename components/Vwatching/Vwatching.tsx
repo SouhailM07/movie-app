a27e659de8b5428eb6182eecbd1afe2a6b0a774b
shadow <lightshadow416@gmail.com>
2024-05-08 14:39:21 +0200

    [Vwatching] component was created

---
+++ components/Vwatching/Vwatching.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { vwatchingStyles } from "./vwatchingStyles.ts";
+import tw from "../../lib/tailwind.js";
+export default function Vwatching() {
+  return (
+    <>
+      <View>
+        <Text>Vwatching</Text>
+      </View>
+    </>
+  );
+}
