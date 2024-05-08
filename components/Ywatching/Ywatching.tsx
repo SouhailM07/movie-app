374feb4b27ad36a1e5b401df8441b86fb13d0502
shadow <lightshadow416@gmail.com>
2024-05-08 14:18:44 +0200

    [Ywatching] component was created

---
+++ components/Ywatching/Ywatching.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { ywatchingStyles } from "./ywatchingStyles.ts";
+import tw from "../../lib/tailwind.js";
+export default function Ywatching() {
+  return (
+    <>
+      <View>
+        <Text>Ywatching</Text>
+      </View>
+    </>
+  );
+}
