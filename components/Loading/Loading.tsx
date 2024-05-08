fb7aed00cc1c81d169c07228578deba2f4f3ad42
shadow <lightshadow416@gmail.com>
2024-05-08 12:58:37 +0200

    [Loading] component was created

---
+++ components/Loading/Loading.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { loadingStyles } from "./loadingStyles.ts";
+import tw from "../../lib/tailwind.js";
+export default function Loading() {
+  return (
+    <>
+      <View>
+        <Text>Loading</Text>
+      </View>
+    </>
+  );
+}
